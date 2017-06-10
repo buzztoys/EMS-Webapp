e619b440d12bc815e91077b8a074af0c9ee7ee35
miyakow <miyako.wilson@gatech.edu>
2017-06-09 21:27:22 -0400

    Add TableService.readTable to read new c3plot format

--- src/services/TableService.js
+++ src/services/TableService.js
@@ -99,6 +99,7 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
       return deferred.promise;
     };
 
+    //var reqOb = {elementId: scope.mmsEid, projectId: projectId, refId: refId, commitId: commitId};
     var readTables = function(reqOb, ws, version) {
       var deferred = $q.defer();
 
@@ -107,8 +108,6 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
 
         console.log("TableService.readTables.data....");
         console.log(data);
-
-
 
         var tableTitles = []; //used only for display
         var tableIds = []; //used as filter id
@@ -119,22 +118,17 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
         rowHeadersMmsEid.elementIds =[];
         rowHeadersMmsEid.projectId = reqOb.projectId;
         rowHeadersMmsEid.refId = reqOb.refId;
-        rowHeadersMmsEid.commitId = reqOb.commitId;
-    
-
-        var dataValuesMmmEid =[];
+        rowHeadersMmsEid.commitId = tableIds;
+        var dataValuesMmmEid=[];
         dataValuesMmmEid.elementIds = [];
         dataValuesMmmEid.projectId = reqOb.projectId;
         dataValuesMmmEid.refId = reqOb.refId;
         dataValuesMmmEid.commitId = reqOb.commitId;
 
-
         var columnCounter;
         var i, j, k;
 
-        console.log("data._contents");
-        console.log(data._contents);
-
+     
         if ( data._contents !==  undefined){ //use contents if exist
         //if ( data.specialization.contains ===  undefined){  
           var tempMmsEid = [];
@@ -183,49 +177,7 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
               readTablesCommon();
             });
         }
-        /*else {
-          console.log("data............");
-          console.log(data);
-
-          var tableContains = [];
-          for ( k = 0; k < data.contains.length; k++ ){
-            if ( data.contains[k].type ==="Table"){
-              //use table title or text before the table as tableTitles
-              if ( data.contains[k].title !== undefined &&  data.contains[k].title.length !== 0)
-                tableTitles.push(toValidId(data.contains[k].title)); 
-              else if ( data.contains[k-1].sourceType==="text")
-                tableTitles.push(toValidId(data.contains[k-1].text.replace("<p>","").replace("</p>","").replace(" ", ""))); //assume it is Paragraph
-              else
-                tableTitles.push("");
-
-              tableIds.push("_" + Math.floor((Math.random() * 1000) + 1)); //a random between 1 and 1000
-              tableContains.push(data.contains[k]);
-              var columnHeaders = [];
-              //assume first column is empty
-              for ( var kk = 1; kk < data.contains[k].header[0].length; kk++){
-                columnHeaders[kk-1] = data.contains[k].header[0][kk].content[0].text.replace("<p>","").replace("</p>","");
-              }  
-              tableColumnHeadersLabels.push(columnHeaders); //xxx, yyy, mass,cost, power in string
-            }
-          }
-          var body;
-          
-          for ( k = 0; k < tableContains.length; k++){
-              body = tableContains[k].body;
-              numOfRowHeadersPerTable.push( body.length);
-              for (i = 0; i < body.length; i++ ){
-                rowHeadersMmsEid.elementIds.push(body[i][0].content[0].source);
-                for ( j = 1; j < body[i].length; j++){
-                  dataValuesMmmEid.elementIds.push(body[i][j].content[0].source);
-                if ( i === 0)
-                  columnCounter = body[i].length - 1;// -1 to remove row header
-              }
-              numOfDataColumn.push(columnCounter);
-            }
-          } //end of for k
-          readTablesCommon();
-        }*/
-
+ 
         //requires numOfRowHeadersPerTable, rowHeadersMmsEid, dataValuesMmmEid, tableIds(to get number of tables)
         //common function to read 2.2 and previous version JSON
         function readTablesCommon(){
@@ -235,7 +187,7 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
           .then(function(rowHeaders) {
               ElementService.getElements(dataValuesMmmEid, 1, false)
                 .then(function(values) {
-                  var dataIdFilters=[];
+                  
                   var dataTableValues = [];
                   var datavalues = [];
                   var startIndex = 0;
@@ -276,15 +228,15 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
                     }
                     filters.push(filterRowHeaders);
                     filters.push(filterColumnHeaders); 
-                    dataIdFilters[tableIds[k]]=filters;
+                    
                   }
                   var r =  {
                      tableTitles: tableTitles,
                      tableIds: tableIds,//[]ss
                      tableColumnHeadersLabels: tableColumnHeadersLabels, //[]
                      tableRowHeaders: tableRowHeaders,
-                     datavalues: dataTableValues, //[][] - array
-                     dataIdFilters: dataIdFilters
+                     datavalues: dataTableValues //[][] - array
+                     
                   };
                 deferred.resolve(r);
             });//ElementService.getElements - dataValuesMmEid
@@ -296,11 +248,87 @@ function TableService($q, $http, URLService, UtilsService, CacheService, _, Elem
       return deferred.promise;
     }; //end of getTables
 
+    var readTable = function(reqOb, ws, version) {
+      var deferred = $q.defer();
+
+    
+      var tableColumnHeadersLabels=[];
+      var i, j, k;
+      //assuming td.type = "Table";
+      if ( reqOb.tableData.header !== undefined){
+        for (i = 1; i < reqOb.tableData.header[0].length; i++ ){
+          tableColumnHeadersLabels.push(reqOb.tableData.header[0][i].content[0].text.replace("<p>","").replace("</p>","").replace(" ", ""));    
+        }
+       }
+      var rowHeadersMmsEid = [];
+      rowHeadersMmsEid.elementIds =[];
+      rowHeadersMmsEid.projectId = reqOb.projectId;
+      rowHeadersMmsEid.refId = reqOb.refId;
+      rowHeadersMmsEid.commitId = reqOb.commitId;
+      var dataValuesMmmEid = [];
+      dataValuesMmmEid.elementIds = [];
+      dataValuesMmmEid.projectId = reqOb.projectId;
+      dataValuesMmmEid.refId = reqOb.refId;
+      dataValuesMmmEid.commitId = reqOb.commitId;
+
+      var numOfDataColumn; 
+      
+      for ( i = 0; i < reqOb.tableData.body.length; i++){ 
+        rowHeadersMmsEid.elementIds.push(reqOb.tableData.body[i][0].content[0].source);
+        for ( j = 1; j < reqOb.tableData.body[i].length; j++ ){
+          dataValuesMmmEid.elementIds.push(reqOb.tableData.body[i][j].content[0].source);
+        }
+        if (i === 0)
+          numOfDataColumn = reqOb.tableData.body[i].length - 1; //-1 to remove row header
+      }
+      var numOfRowHeadersPerTable = reqOb.tableData.body.length;
+      readTablesCommon();
+    
+        function readTablesCommon(){
+          
+          ElementService.getElements(rowHeadersMmsEid, 1, false)
+          .then(function(rowHeaders) {
+              ElementService.getElements(dataValuesMmmEid, 1, false)
+                .then(function(values) {
+                  var dataIdFilters=[];
+                  var dataTableValues = [];
+                  var datavalues = [];
+                  var startIndex = 0;
+                  var counter = 0;
+                  
+                  var valueLength = numOfDataColumn* numOfRowHeadersPerTable;//rowHeadersMmsEid.length;
+                  for (var i = 0; i < valueLength; i= i + numOfDataColumn){
+                    var datarow =[];// new Array(tableColumnHeadersLabels[k].length);
+                    for ( var j = 0; j < numOfDataColumn; j++){
+                      datarow.push(values[counter++]); 
+                    }
+                    datavalues.push(datarow);
+                  }
+
+                  var eachRowHeader =[];
+                    counter = 0;
+                    console.log( "i " + i  + "counter " + counter ); 
+                    for ( i = 0; i < numOfRowHeadersPerTable; i++){
+                      eachRowHeader.push(rowHeaders[counter++]);
+                    }
+                  var r =  {
+                     tableColumnHeadersLabels: tableColumnHeadersLabels, //[]
+                     tableRowHeaders: eachRowHeader,
+                     datavalues: datavalues, //[][] - array
+                  };
+                deferred.resolve(r);
+            });//ElementService.getElements - dataValuesMmEid
+          });//ElementService.getElements - rowHeadersMmsEid
+        } //end of readTableCommon function
+     
+      return deferred.promise;
+    }; //end of readTable
 
     return {
         readTables: readTables,
         toValidId: toValidId,
-        readTableCols: readTableCols
+        readTableCols: readTableCols,
+        readTable: readTable
     };
 
 }
